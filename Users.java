df594f0bcda62db3ff3ce1434d09499d78d75158
Kyle52520 <85924397+Kyle52520@users.noreply.github.com>
2021-06-29 17:10:44 +0800

    added user select command
    added user select command

--- Users.java
+++ Users.java
@@ -9,6 +9,7 @@ import java.util.*;
 public class Users {
     
     public static ArrayList<String> names = new ArrayList<String>();
+    public static String currentUser;
     
     public static String list() {
         
@@ -35,6 +36,27 @@ public class Users {
         System.out.println("\nPlease enter the username:");
         String input = inputs.nextLine();
         names.add(input);
+        
+        return "";
+    }
+    
+    public static String select() {
+        Scanner inputs = new Scanner(System.in);
+        System.out.println("\nPlease enter the username:");
+        String input = inputs.nextLine();
+        int success =0;
+        for(String name : names){
+            if(name.equals(input)){
+                System.out.println("\nWelcome user : "+input);
+                currentUser=input;
+                success=1;
+            }
+            
+        
+        }
+        if(success==0){
+                System.out.println("\n"+input+" is not in the list of users.");
+            }
         
         return "";
     }
